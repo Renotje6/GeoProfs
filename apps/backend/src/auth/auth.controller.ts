49982d7d12661e0629b516e32b5248eaa7e2f719
MilanMaPangPang <120579337+MilanMaPangPang@users.noreply.github.com>
2024-12-04 11:55:58 +0100

    added passport authentication

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,21 +1,31 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, NotImplementedException, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthGuard } from './guards/auth.guard';
-import { userInfo } from 'os';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotImplementedException,
+  Post,
+  Request,
+  UseGuards,
+} from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthGuard } from "./guards/auth.guard";
+import { userInfo } from "os";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
-    constructor(private authService: AuthService){}
-    @HttpCode(HttpStatus.OK)
-    @Post('login')
-    login(@Body() input: {username: string; password: string}){
-        return this.authService.authenticate(input);
-    }
+  constructor(private authService: AuthService) {}
+  @HttpCode(HttpStatus.OK)
+  @Post("login")
+  login(@Body() input: { username: string; password: string }) {
+    return this.authService.authenticate(input);
+  }
 
-    @UseGuards (AuthGuard)
-    // this endpoint in protected by AuthGuard
-    @Get('me')
-    GetUserInfo(@Request() request){
-        return request.user
-    }
+  @UseGuards(AuthGuard)
+  // this endpoint in protected by AuthGuard
+  @Get("me")
+  GetUserInfo(@Request() request) {
+    return request.user;
+  }
 }
--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,21 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { UsersModule } from 'src/users/users.module';
-import { JwtModule } from '@nestjs/jwt';
-import { JWT_SECRET } from './configs/jwt-secret';
-
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { UsersModule } from "src/users/users.module";
+import { JwtModule } from "@nestjs/jwt";
+import { JWT_SECRET } from "./configs/jwt-secret";
+import { PassportModule } from "@nestjs/passport";
+import { PassportAuthController } from "./passport.auth.controller";
+import { LocalStrategy } from "./strategies/local.strategy";
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
-  imports: [UsersModule,
+  controllers: [AuthController, PassportAuthController],
+  providers: [AuthService, LocalStrategy],
+  imports: [
+    UsersModule,
     JwtModule.register({
-      global: true, 
+      global: true,
       secret: JWT_SECRET,
-      signOptions: {expiresIn: '1d'},
-
-    })
-  ]
+      signOptions: { expiresIn: "1d" },
+    }),
+    PassportModule,
+  ],
 })
 export class AuthModule {}
