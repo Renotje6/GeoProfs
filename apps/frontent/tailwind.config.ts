267da51596eb372ad282ebdb41f660ccdca2fce8
Default <j.rehmannski@gmail.com>
2024-12-10 10:41:51 +0100

    fixed tailwind paths

--- apps/frontent/tailwind.config.ts
+++ apps/frontent/tailwind.config.ts
@@ -1,27 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        button: "#006FEE",
-        customGray: '#A7A7A7',
-        hero: "f1f0f0",
-        achterground:"#e7e5e5",
-        headerground : "#eae7e7",
-        buttonsh : "#d1e5fc",
-        pcolor: "#A4A4A4",
-        
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}', '../../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+				button: '#006FEE',
+				customGray: '#A7A7A7',
+				hero: 'f1f0f0',
+				achterground: '#e7e5e5',
+				headerground: '#eae7e7',
+				buttonsh: '#d1e5fc',
+				pcolor: '#A4A4A4',
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
