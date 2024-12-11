031e9b95900f1440efb2ea30166001bf9cb98e3e
MilanMaPangPang <120579337+MilanMaPangPang@users.noreply.github.com>
2024-12-11 14:28:58 +0100

    added swagger setup

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { setupSwagger } from "./swagger.config";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 8080);
+  const app = await NestFactory.create(AppModule);
+
+  // Gebruik de aparte Swagger-configuratie
+  setupSwagger(app);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
