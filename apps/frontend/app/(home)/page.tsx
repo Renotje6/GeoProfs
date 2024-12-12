0f0dc519302913f4acbc33f470e597627fc3bd56
Default <j.rehmannski@gmail.com>
2024-12-12 09:07:20 +0100

    Update page.tsx

--- apps/frontend/app/(home)/page.tsx
+++ apps/frontend/app/(home)/page.tsx
@@ -1,12 +1,6 @@
 export default function Home() {
 	return (
-		// <div className='hero-section flex justify-start items-center mt-16 w-full overflow-hidden'>
-		// 	<div className='hero w-full flex flex-col justify-center items-center py-20'>
-		// 		<h2 className='text-4xl font-bold tracking-wide'>VERLOF MANAGEMENT</h2>
-		// 		<p className='mt-4 text-customGray'>Eenvoudig je verlof beheren, altijd en overal.</p>
-		// 	</div>
-		// </div>
-		<div className='bg-black/5 w-[calc(100%+100px)] -rotate-3 h-80 flex justify-center shadow-xl shadow-black/5'>
+		<div className='bg-black/5 w-[calc(100%+100px)] -mx-[50px] -rotate-3 h-80 flex justify-center shadow-xl shadow-black/5'>
 			<div className='flex justify-center flex-col text-center'>
 				<h1 className='text-4xl font-bold tracking-wide rotate-3'>VERLOF MANAGEMENT</h1>
 				<p className='mt-4 text-xl text-customGray rotate-3'>Eenvoudig je verlof beheren, altijd en overal.</p>
