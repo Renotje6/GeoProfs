be2e4b3ff6e09ff77d2de8397d39c43e470a6ad8
Hussein <omerhussen603@gmail.com>
2025-01-07 11:38:16 +0100

    Add StoryBook

---
+++ apps/frontend/stories/CustomLink.stories.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Meta, StoryFn } from "@storybook/react"; // Gebruik Meta en StoryFn
+import CustomLink from "../app/compontenten/CustomLink";
+
+
+export default {
+  title: "Components/CustomLink",
+  component: CustomLink,
+  argTypes: {
+    href: { control: "text" },
+    text: { control: "text" },
+    height: { control: "text" },
+    width: { control: "text" },
+    className: { control: "text" },
+  },
+} as Meta<typeof CustomLink>;
+
+const Template: StoryFn<typeof CustomLink> = (args) => <CustomLink {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  href: "/",
+  text: "Default Link",
+  height: "40px",
+  width: "150px",
+  className: "inline-block text-center bg-button text-white rounded-xl btn",
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  href: "/large",
+  text: "Large Link",
+  height: "60px",
+  width: "200px",
+  className: "bg-green-500",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  href: "/small",
+  text: "Small Link",
+  height: "30px",
+  width: "100px",
+  className: "bg-red-500",
+};
