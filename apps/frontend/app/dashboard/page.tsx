8037e011a17b5a4c0608ad9f9f225ea8ee01746c
Default <j.rehmannski@gmail.com>
2025-01-07 10:45:36 +0100

    updated dashboard page

--- apps/frontend/app/dashboard/page.tsx
+++ apps/frontend/app/dashboard/page.tsx
@@ -1,28 +1,34 @@
 'use client';
 
+import AbsentionTable from '@/components/dashboard/absention-table';
+import RequestsTable from '@/components/dashboard/requests-table';
+import UsersTable from '@/components/dashboard/users-table';
 import { Button } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
 
 export default function AdminPage() {
+	const router = useRouter();
+
 	const sections = [
 		{
 			key: 'aanvragen',
 			label: 'AANVRAGEN',
-			content: <div>content</div>,
+			content: <RequestsTable />,
 		},
 		{
 			key: 'absentie',
 			label: 'ABSENTIE',
-			content: <div>content</div>,
+			content: <AbsentionTable />,
 		},
 		{
 			key: 'gebruikers',
 			label: 'GEBRUIKERS',
-			content: <div>content</div>,
+			content: <UsersTable />,
 		},
 	];
 
 	return (
-		<div className='flex gap-10 w-full xl:w-[80%] justify-between'>
+		<div className='flex gap-10 w-full xl:w-[80%] justify-between flex-grow-0'>
 			<div className='bg w-96 hidden 2xl:flex items-center flex-col lg:p-4 p-2 gap-2 grow-0 bg-black/5 rounded-xl'>
 				<h2 className='text-xl font-medium'>SECTIONS</h2>
 				{sections.map((section) => (
@@ -30,17 +36,24 @@ export default function AdminPage() {
 						key={section.key}
 						size='sm'
 						className='w-full'
-						onClick={() => {}}>
+						onClick={() => {
+							router.push(`#${section.key}`);
+							document.getElementById(section.key)?.scrollIntoView({
+								behavior: 'smooth',
+							});
+						}}>
 						{section.label}
 					</Button>
 				))}
 			</div>
-			<div className='flex flex-col h-full w-full gap-5 md:gap-10 bg-black/5 rounded-xl p-2 lg:p-4 '>
+			<div className='flex flex-col h-full w-full gap-5'>
 				{sections.map((section) => (
 					<section
+						className='md:gap-10 bg-black/5 rounded-xl p-2 lg:p-4 '
 						key={section.key}
-						id={section.key}
-					/>
+						id={section.key}>
+						<div>{section.content}</div>
+					</section>
 				))}
 			</div>
 		</div>
