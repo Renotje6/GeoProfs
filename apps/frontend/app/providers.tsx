eded39973409ef47591ebda9629356bf7e13744d
Default <j.rehmannski@gmail.com>
2025-01-23 10:22:47 +0100

    added login feature

--- apps/frontend/app/providers.tsx
+++ apps/frontend/app/providers.tsx
@@ -1,8 +1,13 @@
-import { NextUIProvider } from "@nextui-org/react";
-import type { ReactNode } from "react";
+import ReactQueryProvider from '@/components/providers/react-query';
+import { NextUIProvider } from '@nextui-org/react';
+import type { ReactNode } from 'react';
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <NextUIProvider>{children}</NextUIProvider>;
+	return (
+		<NextUIProvider>
+			<ReactQueryProvider>{children}</ReactQueryProvider>
+		</NextUIProvider>
+	);
 };
 
 export default Providers;
