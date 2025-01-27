0b46a4110945016e2b411a4e38bd17da29f4c29e
Renotje6 <reno.rovers@gmail.com>
2025-01-27 21:59:41 +0100

    swagger docs

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,19 +1,22 @@
-import { NestFactory } from "@nestjs/core";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { GetAllSickReportsDepartmentDto } from './sick-reports/dto/responses/getAllSickReportsDepartment.dto';
+import { GetOwnSickReportsDto } from './sick-reports/dto/responses/getOwnSickReports.dto';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	const config = new DocumentBuilder()
-		.setTitle("Geoprofs API")
-		.setDescription(
-			"The API for the Geoprofs portal, allowing employees to easily manage their leave and illness. Managers and administrators can use this API to approve leave requests and monitor absenteeism.",
-		)
-		.setVersion("1.0")
+		.setTitle('Geoprofs API')
+		.setDescription('The API for the Geoprofs portal, allowing employees to easily manage their leave and illness. Managers and administrators can use this API to approve leave requests and monitor absenteeism.')
+		.setVersion('1.0')
 		.build();
-	const documentFactory = () => SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup("api", app, documentFactory);
+	const documentFactory = () =>
+		SwaggerModule.createDocument(app, config, {
+			extraModels: [GetAllSickReportsDepartmentDto, GetOwnSickReportsDto],
+		});
+	SwaggerModule.setup('api', app, documentFactory);
 
 	await app.listen(process.env.PORT ?? 8080);
 }
