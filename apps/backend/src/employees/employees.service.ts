aea40acef470f267ba6256e706c200ce22fe2be9
Renotje6 <reno.rovers@gmail.com>
2025-01-27 22:29:13 +0100

    employee endpoint

--- apps/backend/src/employees/employees.service.ts
+++ apps/backend/src/employees/employees.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Role } from "src/auth/enums/role.enum";
-import { Department } from "src/entities/department.entity";
-import { Employee } from "src/entities/employee.entity";
-import { User } from "src/entities/user.entity";
-import { Repository } from "typeorm";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Department } from '../entities/department.entity';
+import { Employee } from '../entities/employee.entity';
+import { Manager } from '../entities/manager.entity';
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class EmployeesService {
@@ -15,10 +15,14 @@ export class EmployeesService {
 		private departmentRepository: Repository<Department>,
 		@InjectRepository(User)
 		private userRepository: Repository<User>,
+		@InjectRepository(Manager)
+		private managerRepository: Repository<Manager>
 	) {}
 
-	findAll(): Promise<Employee[]> {
-		return this.employeesRepository.find();
+	async findAll(user: User): Promise<Employee[]> {
+		const manager = await this.managerRepository.createQueryBuilder('manager').leftJoinAndSelect('manager.department', 'department').where('manager.userId = :managerId', { managerId: user.id }).getOne();
+
+		return this.employeesRepository.createQueryBuilder('employee').leftJoinAndSelect('employee.user', 'user').where('employee.departmentId = :departmentId', { departmentId: manager.department.id }).getMany();
 	}
 
 	findOne(id: string): Promise<Employee> {
@@ -31,42 +35,10 @@ export class EmployeesService {
 
 	findByEmail(email: string): Promise<Employee | null> {
 		return this.employeesRepository
-			.createQueryBuilder("employee")
-			.leftJoinAndSelect("employee.id", "user") // Join the `User` entity
-			.leftJoinAndSelect("employee.department", "department") // Join the `Department` entity (if needed)
-			.where("user.email = :email", { email }) // Filter by `User.email`
+			.createQueryBuilder('employee')
+			.leftJoinAndSelect('employee.id', 'user') // Join the `User` entity
+			.leftJoinAndSelect('employee.department', 'department') // Join the `Department` entity (if needed)
+			.where('user.email = :email', { email }) // Filter by `User.email`
 			.getOne(); // Get the single result
-	}
-
-	async insert(employee: {
-		email: string;
-		password: string;
-		avatar: string;
-		name: string;
-		departmentId: string;
-	}): Promise<Employee> {
-		if (await this.findByEmail(employee.email)) {
-			throw new Error("Employee already exists");
-		}
-
-		const department = await this.departmentRepository.findOne({
-			where: { id: employee.departmentId },
-		});
-
-		if (!department) {
-			throw new Error("Department not found");
-		}
-
-		const newUser = await this.userRepository.save(
-			this.userRepository.create({ ...employee, role: Role.employee }),
-		);
-
-		return this.employeesRepository.save(
-			this.employeesRepository.create({
-				id: newUser.id,
-				department,
-				balance: 0,
-			}),
-		);
 	}
 }
