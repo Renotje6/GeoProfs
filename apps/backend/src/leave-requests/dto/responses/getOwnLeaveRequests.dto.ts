f171e2e4c5e63cbcce9c83f2eb00bfedf15c40e2
Renotje6 <reno.rovers@gmail.com>
2025-01-30 10:39:31 +0100

    update: fix the getOwnLeaveRequests.dto

--- apps/backend/src/leave-requests/dto/responses/getOwnLeaveRequests.dto.ts
+++ apps/backend/src/leave-requests/dto/responses/getOwnLeaveRequests.dto.ts
@@ -1,4 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { Role } from "src/auth/enums/role.enum";
+import { Employee } from "src/entities/employee.entity";
 import { LeaveRequestStatus, LeaveRequestType } from "src/entities/leave-request.entity";
 
 export class GetOwnLeaveRequestsDto {
@@ -49,4 +51,36 @@ export class GetOwnLeaveRequestsDto {
         example: 'holiday',
     })
     type: LeaveRequestType;
+
+    @ApiProperty({
+            example: 
+                {
+                        id: '1234-1234-1234',
+                        createdAt: '2021-09-01T00:00:00.000Z',
+                        updatedAt: '2021-09-01T00:00:00.000Z',
+                        balance: 10,
+                        user: {
+                            id: '1234-1234-1234',
+                            createdAt: '2021-09-01T00:00:00.000Z',
+                            updatedAt: '2021-09-01T00:00:00.000Z',
+                            name: 'John Doe',
+                            email: 'john.doe@mail.com',
+                            avatar: 'https://example.com/avatar.jpg',
+                            role: 'employee'
+                        }
+                },
+            
+            description: 'The found leave requests',
+        })
+            employee: {
+                user: {
+                    id: string;
+                    createdAt: string;
+                    updatedAt: string;
+                    name: string;
+                    email:string;
+                    avatar:	string;
+                    role: Role
+                }
+            }& Omit<Employee, 'password' | 'user'>;
 }
